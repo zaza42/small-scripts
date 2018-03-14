8bcda534fa6fcf0404a5d29a4d66f7dd851d5ebc
DC-1 (Zaza) <DC-1@tecra.flerp>
2018-03-14 16:49:32 +0100

    browser language

--- xpropview.sh
+++ xpropview.sh
@@ -24,32 +24,132 @@ function audplugins () {
 	wait
     fi
 }
+function xxkbrestart () {
+		echo -n "\n"$(mytime "xxkb restart")
+		pkill -x -13 xxkb >/dev/null ; xxkb &
+#		pkill -x -13 xxkb >/dev/null ; sleep 0.1 ; xxkb &
+#		sleep 1 && icesh -c claws-mail setState Hidden add
+}
+function browserlangcheck () {
+	    case $class in
+		    chromium|palemoon|basilisk|firefox)
+			wmname=${a##*::: }
+			if [ "${wmname:0:4}" = "http" ]; then
+                                case $wmname in
+                            	    *stackexchange*|*codegold*|*stackoverflow*\
+				    |*superuser*|*serverfault*|*github*\
+				    |*debian.org* ) lang="0"
+                            	    ;;
+                            	    *) lang="1"
+                            	    ;;
+				esac
+				langset
+				oldname=$a
+			fi
+	    esac
+}
+function langset () {
+	    if [ $lang -ne 2 ] && [ $(xkblayout-state print "%c ") -ne $lang ]; then
+                case $lang in
+                    0) xkblayout-state set $lang 2>/dev/null; echo -n "\n"$(mytime "lang: us") >&2
+		    ;;
+                    1) xkblayout-state set $lang 2>/dev/null; echo -n "\n"$(mytime "lang: hu") >&2
+		    ;;
+                esac
+		oldlang=$lang
+            fi
+}
+function fulldo () {
+	if [ -n "$isfull" ] && [ $isfull -ne $oldfull ] ; then
+	      echo -n "\n"$(mytime isfull: $isfull)
+	      echo -n "\n"$(mytime isfull: $isfull) >&2
+	      oldfull=$isfull
+	      case $isfull in
+		1)
+				dunstify DUNST_COMMAND_PAUSE
+#				if $(timeout 1 audtool --playback-playing) ; then
+#				    audplugins off
+#				else
+#				    killall -STOP audacious
+#				fi
+				killall -STOP xautolock 2>/dev/null
+				((projectmrun)) || killall -STOP projectM-pulseaudio 2>/dev/null
+#				((palerun)) || killall -STOP palemoon-bin palemoon plugin-container basilisk-bin basilisk firefox firefox-bin 2>/dev/null
+				xset s off -dpms
+				xset s noblank
+				icesh -window $id setLayer 4
+		;;
+		*)	dunstify DUNST_COMMAND_RESUME
+#			killall -18 xautolock xpenguins xsnow gkrellm audacious xtigervncviewer 2>/dev/null
+			killall -18 xautolock projectM-pulseaudio 2>/dev/null
+			xset $xsetparam
+			xset s blank
+#			if [ "$curdesk" = "$auddesk" ]; then audplugins on;fi
+		;;
+	      esac
+	    fi
+}
+
+function fulle() {
+    id=$1
+    xprop -spy -id $id _NET_WM_STATE _NET_WM_NAME 2>/dev/null|
+#    oldfull=0
+    while read a; do
+#    echo fline "$a" >&2
+        case "$a" in
+	*_NET_WM_STATE_FOCUSED*) true
+	    case "$a" in
+		*_NET_WM_STATE_FULLSCREEN*) isfull=1
+		;;
+		*) isfull=0
+		;;
+	    esac
+	;;
+	*_NET_WM_NAME*)
+		case $(xprop -id $id _NET_WM_STATE) in
+		*_NET_WM_STATE_FOCUSED*)
+		    browserlangcheck
+		esac
+	;;
+	*)  isfull=0
+	    pkill -f -13 "xprop -spy -id $id _NET_WM_STATE"
+#		    exit
+	;;
+        esac
+	fulldo
+    done
+}
 
 echo "xprop started"
-stdbuf -oL xprop -spy -root _NET_ACTIVE_WINDOW _NET_CURRENT_DESKTOP | while read -r line
-    do
-#	    if [ "$oldline" = "$line" ]; then continue; fi
-#	    oldline=$line
-	    id=${line##* }
-	    if [ "${id:0:2}" = "0x" ]; then
-		class=$(xprop -id $id WM_CLASS)
+stdbuf -oL xprop -spy -root _NET_ACTIVE_WINDOW _NET_CLIENT_LIST_STACKING 2>/dev/null \
+| while read -r line; do
+    case $line in
+#	_WIN_SUPPORTING_WM_CHECK*) xxkbrestart
+#	;;
+	_NET_ACTIVE_WINDOW*)
+		id=${line##* }
+		[ "${id:0:2}" = "0x" ] || continue
+		class=$(xprop -id $id WM_CLASS 2>/dev/null)
+		[ -n "$class" ] || continue
 		class=${class#*, }
 		class=${class//[\" ]/}
 		palerun=0
 		projectmrun=0
 		case $class in
-#		    firefox|seamonkey|light|palemoon) killall -18 $class
 		    chromium|palemoon|basilisk|firefox)
 #			    killall -18 palemoon-bin palemoon plugin-container basilisk-bin basilisk firefox-bin firefox chromium 2>/dev/null
 			    palerun=1
 #			    audplugins off
-			    wname=$(xprop -id $id _NET_WM_NAME)
-                                case $wname in
-                            	    *stackexchange*|*codegold*|*stackoverflow*|*superuser*|*serverfault*|*github* ) lang="0"
-                            	    ;;
-                            	    *) lang="1"
-                            	    ;;
-				esac
+#			    wname=$(xprop -id $id _NET_WM_NAME 2>/dev/null)
+#			    browserlangcheck
+#                                case $wname in
+#                            	    *stackexchange*|*codegold*|*stackoverflow*\
+#				    |*superuser*|*serverfault*|*github*|*Issue*\
+#				    |*Debian\ Bug* ) lang="0"
+#                            	    ;;
+#                            	    *) lang="1"
+#                            	    ;;
+#				esac
 		    ;;
 #		    audacious) 
 #			       if [ "$audonscreen" = "off" ]; then
@@ -69,74 +169,40 @@ stdbuf -oL xprop -spy -root _NET_ACTIVE_WINDOW _NET_CURRENT_DESKTOP | while read
 				killall -CONT projectM-pulseaudio
 			        projectmrun=1
 		    ;;
+		    ffplay ) xseticon -id "$id" /home/DC-1/.icons/ffmpeg.png
+		    ;;
 		    *) lang="2"
 		    ;;
 		esac
-
-#		read l1 l2 < /proc/loadavg
-#		[ ${l1/.} -gt 800 ] && kill -9 $(ps -eho pid --sort -rss )
-
-		FS=$(xprop -id $id _NET_WM_STATE)
-		case "$FS" in 
-			*FULLSCREEN*) isfull=1 ;;
-			*) isfull=0 ;;
-		esac
-#		echo FS: $FS isfull=$isfull oldfull=$oldfull
 		read x y <<<$(xdotool getwindowgeometry $id|grep -oP "Position: \K[0-9]*")
 		if [ $x -ge $displayx ]; then
 		    echo -n "\n"$(mytime "2nd_head")
-		    icesh -window $id setState AllWorkspaces 1
+#			1.4.2-vel igy ment:
+#		    icesh -window $id setState AllWorkspaces 1
+#			1.4.3 ota ez a workaround
+		    icesh -window $id setWorkspace 0xFFFFFFFF
 		    icesh -window $id setTrayOption 2
 		fi
-	    else
-		curdesk=$id
-#		if [ "$curdesk" = "$auddesk" ]; then audplugins on;fi
-	    fi
-	    if [ $isfull -ne $oldfull ] ; then
-	      echo -n "\n"$(mytime isfull: $isfull)
-#	      echo oldfull:$oldfull
-	      oldfull=$isfull
-	      case $FS in
-		*FULLSCREEN*)   #case $class in xv|mpv ) sismpv.sh f ;; esac
-				dunstify DUNST_COMMAND_PAUSE
-#				if $(timeout 1 audtool --playback-playing) ; then
-#				    audplugins off
-#				else
-#				    killall -STOP audacious
-#				fi
-				killall -STOP xautolock 2>/dev/null
-				((projectmrun)) || killall -STOP projectM-pulseaudio 2>/dev/null
-#				((palerun)) || killall -STOP palemoon-bin palemoon plugin-container basilisk-bin basilisk firefox firefox-bin 2>/dev/null
-				xset s off -dpms
-				xset s noblank
-				
-		;;
-		*)	dunstify DUNST_COMMAND_RESUME
-#			killall -18 xautolock xpenguins xsnow gkrellm audacious xtigervncviewer 2>/dev/null
-			killall -18 xautolock projectM-pulseaudio 2>/dev/null
-			xset $xsetparam
-			xset s blank
-#			if [ "$curdesk" = "$auddesk" ]; then audplugins on;fi
-		;;
-	      esac
-	    fi
-#	    echo "lastclass=$lastclass class=$class"
-#            && ! [ "$oldlang" = "$lang" ]; then
-#            if ! [ "$lastclass" = "$class" ]; then
-#	    if [ $lang -eq 2 ]; then
-#		mytime "lang: none"
-#	    else
-	    if [ $lang -ne 2 ] && [ $(xkblayout-state print "%c ") -ne $lang ]; then
-                case $lang in
-                    0) xkblayout-state set $lang 2>/dev/null; echo -n "\n"$(mytime "lang: us")
-#			xkblayout-state set $lang 2>/dev/null; mytime "lang: us"
-		    ;;
-                    1) xkblayout-state set $lang 2>/dev/null; echo -n "\n"$(mytime "lang: hu")
-		    ;;
-                esac
-		oldlang=$lang
-#		fi
-            fi
+	;;
+#	_NET_CLIENT_LIST_STACKING*)
+#		id=${line##* }
+#		[ "${id:0:2}" = "0x" ] || continue
+#		FS=$(xprop -id $id _NET_WM_STATE)
+#		case "$FS" in
+#			*FULLSCREEN*) isfull=1 ;;
+#			*) isfull=0 ;;
+#		esac
+#	    echo FS: "$FS"
+#		fulldo
+##	;;
+    esac
+#	    echo xkblang: $(xkblayout-state print "%c ") lang: $lang
+#echo -n "\n langsetelott"
+	    langset
+	    str=$(fulle $id)
+	    i=$((${#str}-1))
+	    oldfull=${str:$i:1}
+
             lastline=$line
             lastclass=$class
     done
