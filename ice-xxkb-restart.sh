da6515f34621ea65f8d4329995175c4ffa0e11ad
DC-1 (Zaza) <DC-1@tecra.flerp>
2018-03-29 09:33:57 +0200

    icewm restart with mpv and glava

--- ice-xxkb-restart.sh
+++ ice-xxkb-restart.sh
@@ -4,8 +4,25 @@ source ~/scripts/mytime.inc
 xprop -spy -root _WIN_SUPPORTING_WM_CHECK 2>/dev/null| while read a; do
 #    echo "# $a"
     mytime "IceWM restart...restarting xxkb"
-    if [ -n "$a" ]; then 
-	pkill -x xxkb; sleep 0.5; xxkb & 
-	sleep 1 && icesh -c claws-mail setState Hidden add
+    if [ -n "$a" ]; then
+	pkill -x xxkb; sleep 1; xxkb &
+	(sleep 1 && icesh -c claws-mail setState Hidden add) &
+	killall icewmbg
+#	sleep 1
+#	killall -9 icewmbg
+#	sleep 1
+	icewmbg &
+	outputs="$(xrandr |grep -c \*)"
+	case "$outputs" in
+	    3) sed -e '/^vo=/s/^.*$/vo=xv/' -i ~/.config/mpv/mpv.conf
+		killall glava
+	    ;;
+	    2) sed -e '/^vo=/s/^.*$/vo=gpu,vdpau,xv,wayland/' -i ~/.config/mpv/mpv.conf
+		killall glava
+	    ;;
+	    1) sed -e '/^vo=/s/^.*$/vo=gpu,vdpau,xv,wayland/' -i ~/.config/mpv/mpv.conf
+		pidof glava 2>/dev/null || glava &
+	    ;;
+	esac
     fi
 done
