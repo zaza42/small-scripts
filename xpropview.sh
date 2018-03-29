d25b9438356743b1f8109c9b7f7e0b0ed2f25764
DC-1 (Zaza) <DC-1@tecra.flerp>
2018-03-29 09:30:51 +0200

    multihead

--- xpropview.sh
+++ xpropview.sh
@@ -11,6 +11,7 @@ typeset -i curdesk
 typeset -i isfull
 typeset -i oldfull=0
 typeset -i oldlang=0
+typeset -i lang=0
 typeset -x audonscreen
 typeset -l xsetparam=$(xset q|sed '/timeout/!d; s/^.*timeout:  \([0-9]*\).*cycle:  \([0-9]*\)$/s \1 \2s00/') #'
 typeset -l displayx=$(xrandr|grep -oP -m1 "   \K.*(?=x.*\*\+$)") #"
@@ -25,80 +26,110 @@ function audplugins () {
     fi
 }
 function xxkbrestart () {
-		echo -n "\n"$(mytime "xxkb restart")
-		pkill -x -13 xxkb >/dev/null ; xxkb &
-#		pkill -x -13 xxkb >/dev/null ; sleep 0.1 ; xxkb &
-#		sleep 1 && icesh -c claws-mail setState Hidden add
-}
-function browserlangcheck () {
-	    case $class in
-		    chromium|palemoon|basilisk|firefox)
-			wmname=${1##*::: }
-			if [ "${wmname:0:4}" = "http" ]; then
-                                case $wmname in
-                            	    *stackexchange*|*codegold*|*stackoverflow*\
-				    |*superuser*|*serverfault*|*github*\
-				    |*debian.*|*pouet* ) lang="0"
-                            	    ;;
-                            	    *) lang="1"
-                            	    ;;
-				esac
-				langset
-			fi
-	    esac
+    echo -n "\n"$(mytime "xxkb restart")
+    pkill -x -13 xxkb >/dev/null ; xxkb &
+#	pkill -x -13 xxkb >/dev/null ; sleep 0.1 ; xxkb &
+#	sleep 1 && icesh -c claws-mail setState Hidden add
 }
 function langset () {
-	    if [ $lang -ne 2 ] && [ $(xkblayout-state print "%c ") -ne $lang ]; then
-                case $lang in
-                    0) xkblayout-state set $lang 2>/dev/null; echo -n "\n"$(mytime "lang: us")
-		    ;;
-                    1) xkblayout-state set $lang 2>/dev/null; echo -n "\n"$(mytime "lang: hu")
-		    ;;
-                esac
-		oldlang=$lang
-            fi
+if [ -n "$lang" ] && [ $lang -ne 2 ] && [ $(xkblayout-state print "%c ") -ne $lang ]; then
+    case $lang in
+        0) xkblayout-state set $lang 2>/dev/null; echo -n "\n"$(mytime "lang: us") ;;
+        1) xkblayout-state set $lang 2>/dev/null; echo -n "\n"$(mytime "lang: hu") ;;
+    esac
+    oldlang=$lang
+fi
+}
+function 2ndhead () {
+		windesk=$(xdotool get_desktop_for_window $id 2>/dev/null)
+		if [ ! "$windesk" = "-1" ]; then
+		    read x y <<<$(xdotool getwindowgeometry $id 2>/dev/null|grep -oP "Position: \K[0-9]*")
+		    x=${x:-0}
+		    if [ -n $x ] && [ $x -ge $displayx ]; then
+			echo -n "\n"$(mytime "2nd_head")
+#			1.4.2-vel igy ment:
+#		    icesh -window $id setState AllWorkspaces 1
+#			1.4.3 ota ez a workaround
+			icesh -window $id setWorkspace 0xFFFFFFFF setTrayOption 2
+#			icesh -window $id setTrayOption 2
+		    fi
+		fi
+}
+function browserlangcheck () {
+    case $class in
+	chromium|palemoon|basilisk|firefox)
+		wmname=${1##*::: }
+		if [ "${wmname:0:4}" = "http" ]; then
+                    case $wmname in
+                	*stackexchange*|*codegold*|*stackoverflow*\
+			|*superuser*|*serverfault*|*github*\
+			|*debian.*|*pouet* ) lang="0"
+                    ;;
+                    *)  lang="1"
+                    ;;
+		    esac
+#		    mytime "lang w: _${wmname}_"
+		    langset
+		fi
+    esac
 }
 function fulldo () {
-	if [ -n "$isfull" ] && [ $isfull -ne $oldfull ] ; then
-	      echo -n "\n"$(mytime isfull: $isfull)
-#	      echo -n "\n"$(mytime isfull: $isfull) >&2
-	      oldfull=$isfull
-	      case $isfull in
-		1)
-				dunstify DUNST_COMMAND_PAUSE
+[ "$class" = "taskbar" ] && isfull=$oldfull
+if [ -n "$isfull" ] && [ $isfull -ne $oldfull ]; then
+#    echo -n "\n"$(mytime isfull: $isfull)
+    echo -n "\n"$isfull
+    echo -n "\n$(mytime full: $isfull) ${class}" >&2
+    oldfull=$isfull
+    case $isfull in
+	1)  dunstify DUNST_COMMAND_PAUSE
 #				if $(timeout 1 audtool --playback-playing) ; then
 #				    audplugins off
 #				else
 #				    killall -STOP audacious
 #				fi
-				killall -STOP xautolock glava 2>/dev/null
-				((projectmrun)) || killall -STOP projectM-pulseaudio 2>/dev/null
-#				((palerun)) || killall -STOP palemoon-bin palemoon plugin-container basilisk-bin basilisk firefox firefox-bin 2>/dev/null
-				xset s off -dpms
-				xset s noblank
-				icesh -window $id setLayer 4
+	    stopapps="xautolock glava"
+	    outputs="$(xrandr |grep -c \*)"
+	    case "$outputs" in
+		1)
+		    ((projectmrun)) || stopapps+=" projectM-pulseaudio"
+		    ((palerun)) || stopapps+=" chromium palemoon-bin palemoon plugin-container libpepflashplayer.so"
 		;;
-		*)	dunstify DUNST_COMMAND_RESUME
-			pidof buckle >/dev/null && bucklerun=true || bucklerun=false
-			$bucklerun && keysound.sh off
-			xte 'keydown Control_L' 'key space' 'keyup Control_L'
-			$bucklerun && keysound.sh on
-#			killall -18 xautolock xpenguins xsnow gkrellm audacious xtigervncviewer 2>/dev/null
-			killall -18 xautolock projectM-pulseaudio glava 2>/dev/null
-			xset $xsetparam
-			xset s blank
-#			if [ "$curdesk" = "$auddesk" ]; then audplugins on;fi
-		;;
-	      esac
-	    fi
+	    esac
+#	    pkill -STOP "$stopapps"
+#	    echo "killall -STOP $stopapps" >&2
+	    killall -STOP $stopapps 2>/dev/null
+	    xset s off -dpms
+	    xset s noblank
+	    icesh -window $id setLayer 4
+	    2ndhead $id
+	;;
+	*)  dunstify DUNST_COMMAND_RESUME
+	    pidof buckle >/dev/null && bucklerun=true || bucklerun=false
+#	    $bucklerun && keysound.sh off
+#	    xte 'keydown Control_L' 'key space' 'keyup Control_L'
+#	    $bucklerun && keysound.sh on
+#	    pkill -CONT "chromium|xautolock|projectM-pulseaudio|glava|palemoon|palemoon-bin|plugin-container"
+	    killall -CONT chromium xautolock projectM-pulseaudio glava \
+		palemoon palemoon-bin plugin-container libpepflashplayer.so \
+		2>/dev/null
+	    xset $xsetparam
+	    xset s blank
+#	    if [ "$curdesk" = "$auddesk" ]; then audplugins on;fi
+	;;
+    esac
+fi
 }
 
 function fulle() {
     id=$1
     local focused
+    class=$(xprop -id $id WM_CLASS 2>/dev/null)
+    class=${class#*, }
+    class=${class//[\" ]/}
+#    isfull=0
     xprop -spy -id $id _NET_WM_STATE _NET_WM_NAME 2>/dev/null|
 #    oldfull=0
-    (while read a; do
+    while read a; do
 #    echo fline "$a" >&2
         case "$a" in
 	*_NET_WM_STATE_FOCUSED*) true
@@ -116,14 +147,19 @@ function fulle() {
 		[ "$focused" = "1" ] && browserlangcheck "$a"
 #		esac
 	;;
-	*) isfull=0
+	*) #isfull=0
+	    #fulldo
 	   focused=0
 	    pkill -f -13 "xprop -spy -id $id _NET_WM_STATE"
 #		    exit
+#	    break
 	;;
         esac
 	fulldo
-    done; isfull=0;fulldo;)
+    done;
+#    echo $isfull;
+#isfull=0;
+fulldo;
 }
 
 echo "xprop started"
@@ -146,6 +182,8 @@ stdbuf -oL xprop -spy -root _NET_ACTIVE_WINDOW _NET_CLIENT_LIST_STACKING 2>/dev/
 #			    killall -18 palemoon-bin palemoon plugin-container basilisk-bin basilisk firefox-bin firefox chromium 2>/dev/null
 			    palerun=1
 #			    audplugins off
+			    wmname="$(xprop -id $id _NET_WM_NAME)"
+			    [ -n "$wmname" ] && browserlangcheck "$wmname"
 		    ;;
 #		    audacious) 
 #			       if [ "$audonscreen" = "off" ]; then
@@ -170,36 +208,30 @@ stdbuf -oL xprop -spy -root _NET_ACTIVE_WINDOW _NET_CLIENT_LIST_STACKING 2>/dev/
 		    *) lang="2"
 		    ;;
 		esac
-		read x y <<<$(xdotool getwindowgeometry $id|grep -oP "Position: \K[0-9]*")
-		if [ $x -ge $displayx ]; then
-		    echo -n "\n"$(mytime "2nd_head")
-#			1.4.2-vel igy ment:
-#		    icesh -window $id setState AllWorkspaces 1
-#			1.4.3 ota ez a workaround
-		    icesh -window $id setWorkspace 0xFFFFFFFF
-		    icesh -window $id setTrayOption 2
-		fi
+		2ndhead $id
 	;;
-#	_NET_CLIENT_LIST_STACKING*)
-#		id=${line##* }
-#		[ "${id:0:2}" = "0x" ] || continue
-#		FS=$(xprop -id $id _NET_WM_STATE)
-#		case "$FS" in
-#			*FULLSCREEN*) isfull=1 ;;
-#			*) isfull=0 ;;
-#		esac
-#	    echo FS: "$FS"
-#		fulldo
-##	;;
+	_NET_CLIENT_LIST_STACKING*)
+		id="${line##* }"
+		[ "${id:0:2}" = "0x" ] || continue
+		FS=$(xprop -id $id _NET_WM_STATE 2>/dev/null)
+#		echo "fse: $isfull - $FS"
+		case "$FS" in
+			*FULLSCREEN*) isfull=1 ;;
+			*) isfull=0 ;;
+		esac
+		fulldo >/dev/null
+	;;
     esac
-#	    echo xkblang: $(xkblayout-state print "%c ") lang: $lang
-#echo -n "\n langsetelott"
+#	    mytime lang2 lang=$lang
 	    langset
-	    fulle $id
-#	    str=$(fulle $id)
+#	    fulle $id
+	    fulldo
+	    str=$(fulle $id)
+#	    fulle $id
 #	    i=$((${#str}-1))
-#	    oldfull=${str:$i:1}
-
+	    oldfull=${str:$((${#str}-1)):1}
+#	    oldfull=0
+	    fulldo
             lastline=$line
             lastclass=$class
-    done
+done
