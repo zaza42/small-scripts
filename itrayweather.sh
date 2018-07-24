fd7c838e41fd51ed6c42ff99f1a67942ba873af5
TiMidity++ MIDI sequencer service <DC-1@tecra.flerp>
2018-07-24 02:51:44 +0200

    rain icon in tray

---
+++ itrayweather.sh
@@ -0,0 +1,33 @@
+#!/bin/lksh
+
+#delay=3
+weatherfile=/home/DC-1/.wmWeatherReports/LHBP.TXT
+#weatherfile=/tmp/test.txt
+while :; do
+    unset icon
+    while read -r line; do
+	if [ "${line:0:7}" = "Weather" ]; then
+	    case "${line#*:}" in
+                *light\ rain*) icon=weather-showers-scattered ;;
+                *rain*) 	icon=weather-showers ;;
+                *thunder*) 	icon=weather-storm ;;
+                *snow*) 	icon=weather-snow ;;
+                *fog*) 		icon=weather-fog ;;
+	    esac
+	    break
+	fi
+    done < "$weatherfile"
+
+    if [ -n "$icon" ]; then
+	echo visible:true
+	sleep 0.2
+	echo icon:$icon
+	sleep 0.2
+	echo tooltip:"$line"
+    else
+	echo visible:false
+    fi
+#    sleep $delay
+    inotifywait -e modify "$weatherfile"
+done | yad --notification --listen \
+    --command "xdg-open https://www.idokep.hu/idokep/budapest"
